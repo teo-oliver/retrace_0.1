2bea50edbebd78e415cfab62145aeaac682fa332
Teo Oliver <teo.liz.oli@gmail.com>
2019-01-13 12:05:23 +0000

    retrace redone v2

--- shifts/static/shifts/messages.js
+++ shifts/static/shifts/messages.js
@@ -1,2 +1,4 @@
-const messages = document.querySelectorAll('alert');
-setTimeout(clearError, 2000);
+// Make a script to make the messages desapear after 2 seconds
+
+// const messages = document.querySelectorAll('alert');
+// setTimeout(clearError, 2000);
